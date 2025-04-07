acf39aa39a08ffc162c6c2f04b8e4a311fec6de0
nakato156 <christianvelasces@gmail.com>
2025-04-07 17:08:45 -0500

    atest model

--- Classes/inference/ModelPred.py
+++ Classes/inference/ModelPred.py
@@ -11,7 +11,7 @@ class MultimodalSignLM:
         :device: The device to run the model on (e.g., 'cuda' or 'cpu').
         """
 
-        self.model = model.to(device)
+        self.model = model
         self.tokenizer = tokenizer
         self.device = device
 
@@ -22,10 +22,14 @@ class MultimodalSignLM:
         # Preprocess the inputs
         keypoints_embeddings = keypoints_embeddings.to(self.device)
         inputs = self.tokenizer(text_input, return_tensors="pt").to(self.device)
-
+        
         sign_embed_tokens = torch.tensor(
             [self._find_closest_token(emb, self.all_embeddings) for emb in keypoints_embeddings[0]]  # embeddings[0] porque es un batch de tamaño 1
         ).unsqueeze(0).to(self.device)
+
+        # Add EOS token at the end of the keypoints
+        eos_token_id = self.tokenizer.eos_token_id
+        sign_embed_tokens = torch.cat([sign_embed_tokens, torch.tensor([[eos_token_id]]).to(self.device)], dim=1)
 
         # Concatenate the token embeddings with the text input
         inputs['input_ids'] = torch.cat([inputs['input_ids'], sign_embed_tokens], dim=1)
@@ -36,8 +40,9 @@ class MultimodalSignLM:
     def generate(self, keypoints_embeddings, text_input:str):
         self.model.eval()
 
+        chat_format = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 07 Apr 2025\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
         # Process the inputs
-        inputs = self.process_inputs(keypoints_embeddings, text_input)
+        inputs = self.process_inputs(keypoints_embeddings, chat_format + text_input)
 
         # Generate the output
         with torch.no_grad():
@@ -47,7 +52,7 @@ class MultimodalSignLM:
             )
 
         # Decode the output
-        decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=False)
         return decoded_output
     
     def _find_closest_token(self, embedding, all_embeddings):
