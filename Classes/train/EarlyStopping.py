50e6c74f73d4c65931a890d74243fd4db34742a3
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-04-08 11:44:06 -0500

    Minor changes

--- Classes/train/EarlyStopping.py
+++ Classes/train/EarlyStopping.py
@@ -3,20 +3,22 @@ import torch
 from pathlib import Path
 
 class EarlyStopping:
-    def __init__(self, path_checkpoints:Path, patience:int=10, threshold:float=0.02, verbose:bool=False):
+    def __init__(self, path_checkpoints:Path, patience:int=10, threshold:float=0.002, verbose:bool=False):
         self.best_loss = float("inf")
         self.patience = patience
+        self._patience = patience
         self.verbose = verbose
         self.path_checkpoints = Path(path_checkpoints)
         self.stop = False
         self.treshold = threshold
 
-    def __call__(self,val_loss):
+    def __call__(self, val_loss):
         if val_loss == np.nan:
             if self.verbose: print("Giorgio desgraciado")
         
         if val_loss < self.best_loss:
             self.best_loss = val_loss
+            self.patience = self._patience
         if val_loss > self.best_loss or abs(val_loss - self.best_loss) < self.treshold:
             self.patience -= 1
         
