f4518510e1b46cd9e0b00dbf87ec507d597326bb
nakato156 <christianvelasces@gmail.com>
2025-04-18 00:15:56 -0500

    modify loss function

--- Classes/train/loss.py
+++ Classes/train/loss.py
@@ -15,9 +15,10 @@ class ImitatorLoss(nn.Module):
         self.cos_sim = nn.CosineSimilarity(dim=-1)
 
     def forward(self, pred, target):
-        l2 = F.mse_loss(pred, target)
+        # l2 = F.mse_loss(pred, target)
 
-        cosine_sim = self.cos_sim(pred, target)
+        cosine_sim = self.cos_sim(F.normalize(pred, p=2, dim=-1), F.normalize(target, p=2, dim=-1))
         cosine_loss = 1 - cosine_sim.mean()
 
-        return (self.alpha * l2) + (self.beta * cosine_loss)
+        # return (self.alpha * l2) + (self.beta * cosine_loss)
+        return cosine_loss
