17bc28a4a4966aafd93721286683bdba3bdad03a
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2025-05-02 15:22:34 -0500

    FixPatch Checkpoint bug

--- src/mslm/checkpoint/manager.py
+++ src/mslm/checkpoint/manager.py
@@ -1,79 +1,77 @@
 import os, json
 import torch
 
-#Save architecture
-#The epoch 1 always has the model.pt as the keys
-#The if the checkpoint has different keys it changes from another
-#The runs are diferent epochs from the same state_dict
+# Save architecture
+# The epoch 1 always has the model.pt as the keys
+# The if the checkpoint has different keys it changes from another
+# The runs are diferent epochs from the same state_dict
 
-#Nomeclature
+# Nomeclature
 # Version    - It is considered if the changes in the architecture are considerable
 # Checkpoint - It could be minor changes and if the architure if the same it is executed in with different run file
 # Runs       - Same architecture different runs or epochs
 # Epoch
 
+
 class CheckpointManager:
-    def __init__(self, base_dir, version, checkpoint, model):
+    def __init__(self, base_dir, version, checkpoint):
         self.base = base_dir
         self.v = version
 
-        #Checks if the checkpoint folder is empty or not
+        # Checks if the checkpoint folder is empty or not
         self.ckpt = checkpoint
-        #self._compare_architecture_checkpoint(model)
+        # self._compare_architecture_checkpoint(model)
 
-        #Checks if the checkpoint folder is empty or not
+        # Checks if the checkpoint folder is empty or not
         self.run = 1
-        #self._check_run_path()
-        
+        # self._check_run_path()
+
     def _path(self, extra=""):
-        p = os.path.join(self.base, "checkpoints", str(self.v), str(self.ckpt), extra)
+        p = os.path.join(self.base, str(self.v), str(self.ckpt), extra)
         os.makedirs(p, exist_ok=True)
         return p
 
     def _run_path(self, extra=""):
-        p = os.path.join(
-            self.base, "checkpoints", str(self.v), str(self.ckpt), str(self.run), extra
-        )
+        p = os.path.join(self.base, str(self.v), str(self.ckpt), str(self.run), extra)
         os.makedirs(p, exist_ok=True)
         return p
 
-    #Checks if there is a run folder
+    # Checks if there is a run folder
     def _check_run_path(self):
         run = 1
-        p = os.path.join(self.base, "checkpoints", str(self.v), str(self.ckpt), str(self.run))
+        p = os.path.join(self.base, str(self.v), str(self.ckpt), str(self.run))
         while os.path.isdir(p):
-            run =+ 1
+            run = +1
         os.makedirs(p, exist_ok=True)
         return run
-        
+
     def _compare_architecture_checkpoint(self, model):
-        #Returns true or false
-        #True if there is a similar architure and the checkpoint
-        #False if there is not a similar architecure and the last checkpoint
+        # Returns true or false
+        # True if there is a similar architure and the checkpoint
+        # False if there is not a similar architecure and the last checkpoint
         self._path()
-        p = os.path.join(self.base, "checkpoints", str(self.v))
-        #Get checkpoints from the versions
+        p = os.path.join(self.base, str(self.v))
+        # Get checkpoints from the versions
         checkpoints_list = os.listdir(p)
-        
+
         state_dict_model = model.state_dict()
         last_checkpoint = 0
 
         if len(checkpoints_list) == 0:
             False
-            
+
         for checkpoint in checkpoints_list:
             checkpoint_path = os.path.join(p, checkpoint)
-            
+
             if last_checkpoint < int(checkpoint):
                 last_checkpoint = int(checkpoint)
 
-            if (
-                os.path.isdir(checkpoint_path)
-                and (os.path.isfile(os.path.join(checkpoint_path, "1", "model.pt"))
-                or os.path.isfile(os.path.join(checkpoint_path, "1", "1", "model.pt")))
+            if os.path.isdir(checkpoint_path) and (
+                os.path.isfile(os.path.join(checkpoint_path, "1", "model.pt"))
+                or os.path.isfile(os.path.join(checkpoint_path, "1", "1", "model.pt"))
             ):
                 model_path = os.path.join(checkpoint_path, "1", "model.pt")
-                
+
                 if os.path.isfile(model_path):
                     model_checkpoint = torch.load(model_path)
                 else:
@@ -91,27 +89,23 @@ class CheckpointManager:
         self.ckpt = int(last_checkpoint) + 1
         return False
 
-    def save_model(self, model, epoch):
-        path = self._path(str(epoch))
-        torch.save(model, os.path.join(path, "model.pt"))
+    def load_checkpoint(self, model):
+        state = torch.load(self._run_path())
+        model.load_state_dict(state)
+        return model
+
+    def save_checkpoint(self, model, epoch):
+        path = self._run_path(str(epoch))
+        torch.save(model.state_dict(), os.path.join(path, "checkpointt.pth"))
 
     def load_model(self, model, path):
         state = torch.load(path)
         model.load_state_dict(state)
         return model
-    
-    def load_model_run(self, model):
-        state = torch.load(self._run_path())
-        model.load_state_dict(state)
-        return model
 
-    def save_model_run(self, model, epoch):
-        path = self._run_path(str(epoch))
-        torch.save(model.state_dict(), os.path.join(path, "model.pth"))
-
-    def save_entire_model(self, model, epoch = 1):
-        path = self._run_path(epoch)
-        torch.save(model.state_dict(), os.path.join(path, "model.pt"))
+    def save_model(self, model, epoch):
+        path = self._path(str(epoch))
+        torch.save(model, os.path.join(path, "model.pt"))
 
     def save_params(self, params):
         p = self._path()
--- src/mslm/utils/paths.py
+++ src/mslm/utils/paths.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 from os import getcwd
 
+
 class PathVariables:
     __slots__ = (
-        '_base_path',
-        'model_path',
-        'report_path',
-        'logs_path',
-        'study_path',
-        'data_path',
-        'h5_file',
-        'csv_file',
+        "_base_path",
+        "model_path",
+        "report_path",
+        "logs_path",
+        "study_path",
+        "data_path",
+        "h5_file",
+        "csv_file",
     )
     _instance = None
 
@@ -22,30 +23,33 @@ class PathVariables:
 
     def _init(self, base_path: str | Path):
         bp = Path(base_path) if base_path else Path(getcwd())
-        if not (bp / "data").exists():
-            raise FileNotFoundError(f"Base path {bp} does not contain the required data directory.")
-        
+        if not (bp.parent / "data").exists():
+            raise FileNotFoundError(
+                f"Base path {bp} does not contain the required data directory."
+            )
+
         self._base_path = bp
 
         out = bp / "outputs"
         out.mkdir(parents=True, exist_ok=True)
 
-        self.model_path  = out / "checkpoints"
+        self.model_path = out / "checkpoints"
         self.model_path.mkdir(exist_ok=True)
-        
+
         self.report_path = out / "reports"
         self.report_path.mkdir(exist_ok=True)
-        
-        self.logs_path   = out / "logs"
+
+        self.logs_path = out / "logs"
         self.logs_path.mkdir(exist_ok=True)
-        
-        self.study_path  = out / "studies"
+
+        self.study_path = out / "studies"
         self.study_path.mkdir(exist_ok=True)
 
         # Datos
-        dp = bp / "data" / "dataset2"
+        dp = bp.parent / "data" / "dataset2"
         self.data_path = dp
-        self.h5_file   = dp / "keypoints.h5"
-        self.csv_file  = dp / "meta.csv"
-    
+        self.h5_file = dp / "keypoints.h5"
+        self.csv_file = dp / "meta.csv"
+
+
 path_vars = PathVariables()
